b0b88eeaaf522f5a2d0b84f89e791960e158c172
denpamusic <denpa@netfleet.space>
2019-02-04 04:29:41 +0300

    Added request object and tests.
    In preparation for request batching support.

---
+++ src/Requests/Request.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denpa\Bitcoin\Requests;
+
+use Denpa\Bitcoin\Client;
+
+class Request
+{
+    /**
+     * Request method.
+     *
+     * @var array
+     */
+    protected $method;
+
+    /**
+     * Request parameters.
+     *
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * Constructs new request.
+     *
+     * @param array|string $config
+     * @param mixed        $params,...
+     *
+     * @return void
+     */
+    public function __construct(string $method, ...$params)
+    {
+        $this->method = $method;
+        $this->params = (array) $params;
+    }
+
+    /**
+     * Gets request method.
+     *
+     * @return string
+     */
+    public function getMethod() : string
+    {
+        return $this->method;
+    }
+
+    /**
+     * Gets request parameters.
+     *
+     * @return array
+     */
+    public function getParams() : array
+    {
+        return $this->params;
+    }
+
+    /**
+     * Sets request parameters.
+     *
+     * @param mixed $params,...
+     *
+     * @return array
+     */
+    public function setParams(...$params) : void
+    {
+        $this->params = (array) $params;
+    }
+
+    /**
+     * Serializes request as array for client.
+     *
+     * @param \Denpa\Bitcoin\Client $client
+     *
+     * @return array
+     */
+    public function serializeFor(Client $client) : array
+    {
+        return [
+            'method' => $client->getConfig()['preserve_case'] ?
+                $this->method : strtolower($this->method),
+            'params' => $this->params,
+            'id'     => $client->getId(),
+        ];
+    }
+}
---
+++ tests/Requests/RequestTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Denpa\Bitcoin\Tests\Request;
+
+use Denpa\Bitcoin\Client as BitcoinClient;
+use Denpa\Bitcoin\Tests\TestCase;
+use Denpa\Bitcoin\Requests\Request;
+
+class RequestTest extends TestCase
+{
+    /**
+     * Set up test.
+     *
+     * @return void
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->request = new Request('getblock', 'foo');
+    }
+
+    /**
+     * Test request method getter.
+     *
+     * @return void
+     */
+    public function testGetMethod() : void
+    {
+        $this->assertEquals('getblock', $this->request->getMethod());
+    }
+
+    /**
+     * Test request params getter.
+     *
+     * @return void
+     */
+    public function testGetParams() : void
+    {
+        $this->assertEquals(['foo'], $this->request->getParams());
+    }
+
+    /**
+     * Test request params setter.
+     *
+     * @return void
+     */
+    public function testSetParams() : void
+    {
+        $this->request->setParams('bar', 'baz');
+
+        $this->assertEquals(['bar', 'baz'], $this->request->getParams());
+    }
+
+    /**
+     * Test request json serializer.
+     *
+     * @return void
+     */
+    public function testSerializeFor() : void
+    {
+        $array = $this->request->serializeFor(new BitcoinClient());
+
+        $this->assertEquals([
+            'method' => 'getblock',
+            'params' => ['foo'],
+            'id' => 0,
+        ], $array);
+    }
+}
